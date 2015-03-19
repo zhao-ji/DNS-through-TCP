84d6a9b6c2448628a59163f2adc18572e602335a
zhao-ji <iamfrommop@gmail.com>
2015-03-19 19:18:07 +0800

    feature: DNS shunt

--- dns_proxy.py
+++ dns_proxy.py
@@ -15,18 +15,28 @@ class ProxyHandler(DatagramRequestHandler):
     '''DNS Proxy Server'''
     def handle(self):
         data, client = self.request
-        logbook.info(
-            "query name: {}"
-            .format(DNSRecord.parse(data).q.qname))
-        send_data = struct.pack("!H", len(data)) + data
-        recv_data = send_tcp(send_data)
-        if recv_data:
-            recv_data = recv_data[2:]
-            ip_list = "\n".join(
-                [str(r.rdata) for r in DNSRecord.parse(recv_data).rr]
-                )
-            logbook.info("record name:\n{}".format(ip_list))
-            client.sendto(recv_data, self.client_address)
+
+        domain = str(DNSRecord.parse(data).q.qname)
+        logbook.info("query name: {}".format(domain))
+
+        basic_domain = ".".join(domain.rstrip(".").split(".")[-2:])
+        logbook.info(basic_domain)
+        if basic_domain in CHINA_DOMAIN_LIST:
+            logbook.info("Go dirty DNS")
+            sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+            sock.sendto(data, DIRTY_DNS)
+            ret_data, _ = sock.recvfrom(8192)
+        else:
+            logbook.info("Go pure DNS")
+            send_data = struct.pack("!H", len(data)) + data
+            recv_data = send_tcp(send_data)
+            ret_data = recv_data[2:]
+
+        ip_list = "\n".join(
+            [str(r.rdata) for r in DNSRecord.parse(ret_data).rr]
+            )
+        logbook.info("record name:\n{}".format(ip_list))
+        client.sendto(ret_data, self.client_address)
 
 def send_tcp(data):
     """
@@ -34,20 +44,20 @@ def send_tcp(data):
         (in/out packets will have prepended TCP length header)
     """
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((args.dns, args.dns_port))
+    sock.connect(PURE_DNS)
     sock.sendall(data)
     response = sock.recv(8192)
-    try:
-        assert len(response) >= 2
-    except AssertionError:
-        sock.close()
-        logbook.error("ops! empty response")
-    else:
-        length = struct.unpack("!H",bytes(response[:2]))[0]
-        while len(response) - 2 < length:
-            response += sock.recv(8192)
-        sock.close()
-        return response
+    # try:
+    #     assert len(response) >= 2
+    # except AssertionError:
+    #     sock.close()
+    #     logbook.error("ops! empty response")
+    # else:
+    length = struct.unpack("!H",bytes(response[:2]))[0]
+    while len(response) - 2 < length:
+        response += sock.recv(8192)
+    sock.close()
+    return response
 
 
 if __name__ == "__main__":
@@ -61,20 +71,30 @@ if __name__ == "__main__":
         metavar="<address>",
         help="Local proxy listen address (default:all)")
     p.add_argument(
-        "--upstream", "-u", default="8.8.8.8:53",
+        "--pure", "-f", default="8.8.8.8:53",
         metavar="<dns server:port>",
-        help="Upstream DNS server:port (default:8.8.8.8:53)")
+        help="Pure foreign DNS server:port (default:8.8.8.8:53)")
+    p.add_argument(
+        "--dirty", "-d", default="114.114.114.114:53",
+        metavar="<dns server:port>",
+        help="Dirty china DNS server:port (default:114.114.114.114:53)")
     args = p.parse_args()
 
-    args.dns, _, args.dns_port = args.upstream.partition(':')
-    args.dns_port = int(args.dns_port or 53)
+    args.pure_dns, _, args.pure_dns_port = \
+        args.pure.partition(':')
+    args.pure_dns_port = int(args.pure_dns_port or 53)
+    args.dirty_dns, _, args.dirty_dns_port = \
+        args.dirty.partition(':')
+    args.dirty_dns_port = int(args.dirty_dns_port or 53)
+
+    PURE_DNS = (args.pure_dns, args.pure_dns_port)
+    DIRTY_DNS = (args.dirty_dns, args.dirty_dns_port)
+
+    from china_domain import china_domain_list as CHINA_DOMAIN_LIST
 
     server = ForkingUDPServer((args.address, args.port), ProxyHandler)
     logbook.info(
         "Start proxy server at {}:{}"
         .format(args.address, args.port))
-    logbook.info(
-        "Connect DNS server at {}:{}"
-        .format(args.dns, args.dns_port))
 
     server.serve_forever()
